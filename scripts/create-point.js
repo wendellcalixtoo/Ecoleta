14e52ee0a1dad7d4add176e05142097b411632c8
wendellcalixtoo <wendellcalixtoo@gmail.com>
2020-06-03 17:26:31 -0300

    Feature: Fetch API

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -22,17 +22,51 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios `
 
+    citySelect.innerHTML= "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true;
+
     fetch(url)
     .then( res => res.json())
     .then( cities => {
+      
+
       for (const city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
       }
 
       citySelect.disabled = false
     });
 }
 
-
-
 document.querySelector('select[name=uf]').addEventListener('change', getCities)
+
+// Itens de coleta
+// Pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem)
+}
+
+let selectedItems = []
+
+function handleSelectedItem(event){
+  const itemLi = event.target
+
+  // adicionar ou remover uma classe com javascript
+  itemLi.classList.toggle("selected")
+
+  const itemId = itemLi.dataset.id
+
+  // Verificar se existem itens selecionados, se sim 
+  // pegar os itens selecionados
+
+  
+
+  // Se ja estiver selecionado, tirar da seleção
+
+  // Se nao estiver selecionado, adicionar à selecção
+
+
+  //atualizar o campo escondido com os items selecionados
+}
