453612824a793d1bc6446ca170de5168654bff4e
wendellcalixtoo <wendellcalixtoo@gmail.com>
2020-06-07 13:17:50 -0300

    DB Connect

--- src/database/db.js
+++ src/database/db.js
@@ -4,52 +4,69 @@ const sqlite3 = require('sqlite3').verbose();
 // iniciar o objeto que irá fazer operações no banco de dados
 const db = new sqlite3.Database('./src/database/database.db');
 
-// Utilizar o objeto de banco de dados, para nossas operações
-db.serialize(() => {
-  // Com comando SQL nos vamos:
-  // 1 - Criar uma tabela
-  db.run(`
-    CREATE TABLE IF NOT EXISTS places (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        image TEXT,
-        name TEXT,
-        address TEXT,
-        address2 TEXT,
-        state TEXT,
-        city TEXT,
-        items TEXT 
-    )
-  `);
-  // 2 - Inserir dados na tabela
-  const query = `
-  INSERT INTO places (
-      image,
-      name,
-      address,
-      address2,
-      state,
-      city,
-      items
-  ) VALUES (?,?,?,?,?,?,?);
-`;
-  const values = [
-    'https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1174&q=80',
-    'Colectoria',
-    'Guilherme Gemballa, Jardim América',
-    'Número 260',
-    'Santa Catarina',
-    'Rio do Sul',
-    'Resíduos Eletrônicos, Lâmpadas'
-  ];
-  function afterInsertData(err){
-    if(err){
-        return console.log(err)
-    }
-    console.log("Cadastrado com sucesso")
-    console.log(this)
-  }
-  db.run(query, values, afterInsertData);
+module.exports = db
 
-  // 3 - Consultar os dados da tabela
-  // 4 - Deletar um dado da tabela
-});
+// Utilizar o objeto de banco de dados, para nossas operações
+// db.serialize(() => {
+//   // Com comando SQL nos vamos:
+//   // 1 - Criar uma tabela
+//   db.run(`
+//     CREATE TABLE IF NOT EXISTS places (
+//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         image TEXT,
+//         name TEXT,
+//         address TEXT,
+//         address2 TEXT,
+//         state TEXT,
+//         city TEXT,
+//         items TEXT 
+//     )
+//   `);
+//   // 2 - Inserir dados na tabela
+//   const query = `
+//   INSERT INTO places (
+//       image,
+//       name,
+//       address,
+//       address2,
+//       state,
+//       city,
+//       items
+//   ) VALUES (?,?,?,?,?,?,?);
+// `;
+//   const values = [
+//     'https://greentumble.com/wp-content/uploads/2015/10/paper-recycling.jpg',
+//     'Paperside',
+//     'Guilherme Gemballa, Jardim América',
+//     'Número 260',
+//     'Santa Catarina',
+//     'Rio do Sul',
+//     'Papéis e Papelão'
+//   ];
+//   function afterInsertData(err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log('Cadastrado com sucesso');
+//     console.log(this);
+//   }
+//     db.run(query, values, afterInsertData);
+
+//   // 3 - Consultar os dados da tabela
+// //   db.all(`SELECT * FROM places`, function (err, rows) {
+// //     if (err) {
+// //       return console.log(err);
+// //     }
+// //     console.log('Aqui estão seus registros');
+// //     console.log(rows);
+// //   });
+
+//   // 4 - Deletar um dado da tabela
+// //   db.run(`DELETE FROM places WHERE id = ?`, [1], function(err){
+// //       if(err){
+// //           return console.log(err)
+// //       }
+
+// //       console.log("Registro deletado com sucesso!")
+// //   })
+// });
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const server = express();
 
+// Pegar o banco de dados
+const db = require('./database/db');
+
 // Configurar pasta publica
 server.use(express.static('public'));
 
@@ -26,7 +29,21 @@ server.get('/create-point', (req, res) => {
 
 // Pagina Search-results
 server.get('/search', (req, res) => {
-  return res.render('search-results.html');
+  // Pegar os dados do banco de dados
+  db.all(`SELECT * FROM places`, function (err, rows) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Aqui estão seus registros');
+    console.log(rows);
+
+    const total = rows.length
+
+    // Mostrar a pagina HTML com os dados do banco de dados
+    return res.render('search-results.html', { places: rows, total: total});
+  });
+
+
 });
 
 // Ligar o servidor
